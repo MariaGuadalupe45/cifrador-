75c37ae8ef1f7cf23b850676cf5d1c8e0d7a5f32
MariaGuadalupe45 <142284162+MariaGuadalupe45@users.noreply.github.com>
2023-08-16 18:17:02 -0600

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-function cifrar() {
-    const alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var mensaje = document.getElementById("mensaje");
-    var resultado = document.getElementById("resultado");
-    let mensaje_original = mensaje.value;
-    for (let i = 0; i < mensaje_original.length; i++) {
-        let letra = mensaje_original[i];
-        let index = alfabeto.indexOf(letra);
-        let newletter = alfabeto[index + 5];
-        mensaje_original = mensaje_original.replaceAll(letra, newletter);
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+function shiftLetter(letter, offsetAmount) {
+    const index = alphabet.indexOf(letter);
+    
+    if (index === -1) {
+        return letter;
+    } else {
+        const newIndex = (index + offsetAmount) % alphabet.length;
+        return alphabet[newIndex];
     }
-    resultado.value = mensaje_original;
 }
 
-function decifrar() {
-    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var menci = document.getElementById("menci");
-    var res = document.getElementById("res");
-    let originalMessage = menci.value;
-    for (let i = 0; i < originalMessage.length; i++) {
-        let letter = originalMessage[i];
-        let index = alphabet.indexOf(letter);
-        let newletter = alphabet[index - 5];
-        originalMessage = originalMessage.replaceAll(letter, newletter);
-    }
-    res.value = originalMessage;
+function cifrar() {
+    const mensajeInput = document.getElementById('mensaje').value.toLowerCase();
+    const offsetAmount = parseInt(document.getElementById('offsetAmount').value);
+    const resultadoOutput = document.getElementById('resultado');
+    
+    const mensajeCifrado = mensajeInput.split('').map(letter => shiftLetter(letter, offsetAmount)).join('');
+    
+    resultadoOutput.value = mensajeCifrado;
+}
+
+function descifrar() {
+    const mensajeInput = document.getElementById('menci').value.toLowerCase();
+    const offsetAmount = parseInt(document.getElementById('offsetAmount2').value);
+    const resultadoOutput = document.getElementById('res');
+    
+    const mensajeOriginal = mensajeInput.split('').map(letter => shiftLetter(letter, -offsetAmount + alphabet.length)).join('');
+    
+    resultadoOutput.value = mensajeOriginal;
 }
